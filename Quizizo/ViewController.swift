ed0f832548bfb2711e39cd70cc98716e2955f2eb
Murad <adigozelov.murad2000@gmail.com>
2025-10-03 14:24:26 +0400

    Initial commit

---
+++ Quizizo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizizo
+//
+//  Created by MURAD on 1.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ QuizizoTests/QuizizoTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuizizoTests.swift
+//  QuizizoTests
+//
+//  Created by MURAD on 1.10.2025.
+//
+
+import Testing
+@testable import Quizizo
+
+struct QuizizoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
